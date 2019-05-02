2f1da085fdd13d7ecc4d0032e6e904dae930adba
Chris Ward <chris.ward@ethereum.org>
2019-05-02 12:32:06 +1000

    Fix paths in script

--- test/docsExamples.sh
+++ test/docsExamples.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
+## GLOBAL VARIABLES
+
+REPO_ROOT=$(cd $(dirname "$0")/.. && pwd)
+
+## FUNCTIONS
+
+if [ "$CIRCLECI" ]
+then
+    function printTask() { echo "$(tput bold)$(tput setaf 2)$1$(tput setaf 7)"; }
+    function printError() { echo "$(tput setaf 1)$1$(tput setaf 7)"; }
+else
+    function printTask() { echo "$(tput bold)$(tput setaf 2)$1$(tput sgr0)"; }
+    function printError() { echo "$(tput setaf 1)$1$(tput sgr0)"; }
+fi
+
 printTask "Checking docs examples style…"
 (
     set -e
